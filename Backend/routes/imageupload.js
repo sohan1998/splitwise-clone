bfb01c3a2501693c8e04c73942fafb2350838a1d
Sohan Shirodkar <sohan1998@gmail.com>
2021-12-14 19:23:44 -0800

    Added Register and Login APIs

--- Backend/routes/imageupload.js
+++ Backend/routes/imageupload.js
@@ -1,71 +1,68 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
-const User = require("../models/User");
-const fs = require("fs");
-var util = require("util");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const User = require('../models/User');
+const fs = require('fs');
+var util = require('util');
 const unlinkFile = util.promisify(fs.unlink);
-const { uploadFile, getFileStream } = require("../services/file-upload");
+const { uploadFile, getFileStream } = require('../services/file-upload');
 
 // const singleUpload= upload.single('image');
 
-router.post("/imageupload", upload.single("file"), async function (req, res) {
-  try {
-    console.log("Inside API");
-    console.log(req.file);
-    const file = req.file;
-    const useremail = req.body.useremail;
-    console.log(useremail);
-    console.log(file.path);
-    //console.log(file);
+router.post('/imageupload', upload.single('file'), async function (req, res) {
+    try {
+        console.log('Inside API');
+        console.log(req.file);
+        const file = req.file;
+        const useremail = req.body.useremail;
+        console.log(useremail);
+        console.log(file.path);
+        //console.log(file);
 
-    // apply filter
-    // resize
+        // apply filter
+        // resize
 
-    const result = await uploadFile(file);
+        const result = await uploadFile(file);
 
-    await unlinkFile(file.path);
-    console.log("Hello");
-    console.log(result);
-    console.log(result.Location);
+        await unlinkFile(file.path);
+        console.log('Hello');
+        console.log(result);
+        console.log(result.Location);
 
-    await User.updateOne(
-      { email: useremail },
-      { $set: { profileImg: result.Location } }
-    );
+        await User.updateOne({ email: useremail }, { $set: { profileImg: result.Location } });
 
-    console.log("I am here");
-    res.status(200).json({ message: "Picture uploaded successfully" });
-  } catch (error) {
-    res.writeHead(400, { "Content-Type": "text/plain" });
-  }
+        console.log('I am here');
+        res.status(200).json({ message: 'Picture uploaded successfully' });
+    } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+    }
 });
 
-router.post("/getimage", async function (req, res) {
-  try {
-    console.log("Inside Get Image API");
-    const useremail = req.body.useremail;
-    console.log(useremail);
-    const userimage = await User.find({ email: useremail }, { profileImg: 1 });
+router.post('/getimage', async function (req, res) {
+    try {
+        console.log('Inside Get Image API');
+        const useremail = req.body.useremail;
+        console.log(useremail);
+        const userimage = await User.find({ email: useremail }, { profileImg: 1 });
 
-    res.status(200).json({ imagelink: userimage });
-  } catch (error) {
-    res.writeHead(400, { "Content-Type": "text/plain" });
-  }
+        res.status(200).json({ imagelink: userimage });
+    } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+    }
 });
 
-router.post("/getimageonload", async function (req, res) {
-  try {
-    console.log("Inside Get Image onload API");
-    const useremail = req.body.useremail;
-    console.log(useremail);
-    const userimage = await User.find({ email: useremail }, { profileImg: 1 });
+router.post('/getimageonload', async function (req, res) {
+    try {
+        console.log('Inside Get Image onload API');
+        const useremail = req.body.useremail;
+        console.log(useremail);
+        const userimage = await User.find({ email: useremail }, { profileImg: 1 });
 
-    res.status(200).json({ imagelink: userimage });
-  } catch (error) {
-    res.writeHead(400, { "Content-Type": "text/plain" });
-  }
+        res.status(200).json({ imagelink: userimage });
+    } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+    }
 });
 
 module.exports = router;
