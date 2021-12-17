19cb700cf2191f026c669ff94c170070b30f6da2
Sohan Shirodkar <sohan1998@gmail.com>
2021-12-17 01:28:12 -0800

    Implemented Sign Up using Redux and MongoDB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/Main';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        // Use Browser Router to route to different pages
+        <BrowserRouter>
+            <div>
+                {/* App Component has a Child Component called Main*/}
+                <Main />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerWorker';
+
+import RootReducer from './reducers';
+
+const middleware = [thunk];
+
+//middleware settings
+// To resolve promise to store we use apply
+const composePlugin = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const store = createStore(RootReducer, composePlugin(applyMiddleware(...middleware)));
+//createStoreWithMiddleware(RootReducer)
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
