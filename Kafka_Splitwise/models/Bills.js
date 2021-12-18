86a01dde22b201fd64b8705f5c8bdc3e22905456
Sohan Shirodkar <sohan1998@gmail.com>
2021-12-17 18:02:20 -0800

    Added Bills model

---
+++ Kafka_Splitwise/models/Bills.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// Create Schema
+const BillSchema = new Schema({
+    billamount: {
+        type: Number,
+        required: true,
+    },
+    billdate: {
+        type: Date,
+        default: Date.now,
+    },
+    billcreatedby: {
+        type: String,
+    },
+    billdescription: {
+        type: String,
+    },
+    groupname: {
+        type: String,
+    },
+    billcomments: {
+        type: Array,
+        default: [],
+    },
+});
+module.exports = User = mongoose.model('bills', BillSchema);
